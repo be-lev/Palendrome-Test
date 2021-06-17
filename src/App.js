9300f04021f1509a1e2420b7cf78e547a70035a0
Lev <levberger19890@gmail.com>
2021-06-17 12:59:47 +0300

    lets train it's importent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [success, setSuccess] = useState(false);
+  const [fail, setFail] = useState(false);
+  let input;
+  let palendrome = [];
+
+  const handleInput = (e) => {
+    input = e.target.value;
+  };
+
+  const reverseWord = (value) => {
+    for (let i = value.length - 1; i > -1; i--) {
+      const elem = value[i];
+      console.log(elem);
+      palendrome.push(elem);
+    }
+  };
+
+  const concatWord = () => {
+    palendrome = palendrome.join("");
+  };
+
+  const confirmMatch = () => {
+    if (input === palendrome) {
+      setSuccess(true);
+      return
+    }
+    setFail(true);
+  };
+
+  const clearPalendrome = () => {
+    palendrome = [];
+  };
+
+  const resetForm = () => {
+      if (fail || success) {
+          setSuccess(false);
+          setFail(false);
+      }
+  };
+  const checkIfPalendrome = (value) => {
+    resetForm();
+    reverseWord(value);
+    concatWord();
+    confirmMatch();
+    clearPalendrome();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="inputArea">
+          <input onChange={(e) => handleInput(e)} type="text" />
+          <button onClick={() => checkIfPalendrome(input)}>Submit</button>
+        </div>
+        {success && <div className="success">That's a palendrome!!</div>}
+        {fail && <div className="fail">Sorry That's NOT a palendrome...</div>}
+      </div>
     </div>
   );
 }
